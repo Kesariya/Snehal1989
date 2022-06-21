997ed6ee8da2ad1de82e5e68e0776e65ae7c633e
91950 <91950@LAPTOP-P4RCEDB1>
2022-06-21 22:49:11 +0530

    Repository class2

---
+++ src/arraylistclasspackage/ArrayListClass.java
@@ -0,0 +1,22 @@
+package arraylistclasspackage;
+
+import java.util.ArrayList;
+
+public class ArrayListClass {
+	
+	public static void main(String[] args) {
+		
+	
+	ArrayList al = new ArrayList();
+			
+	al.add(50);
+	
+	al.add("abc");
+	
+	al.add(true);
+	
+	System.out.println(al);
+	
+	
+	}
+}
